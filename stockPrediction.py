15eff129e6ba9df30178802501c05e17fe436047
Srijit2 <53913645+Srijit2@users.noreply.github.com>
2022-04-03 18:23:08 -0700

    Update stockPrediction.py

--- stockPrediction.py
+++ stockPrediction.py
@@ -19,88 +19,6 @@ from pandas_datareader.data import DataReader
 # For time stamps
 from datetime import datetime
 
-# This method predicts stock prices based on the previous 40 day closing prices.
-def analyze_stock_lstm():
-  stockName = input("enter stock name: ")
-
-  end = datetime.now()
-  start = datetime(end.year - 3, end.month, end.day)
-  ds = DataReader(stockName, 'yahoo', start, end)
-
-  csv = stockName + '.csv'
-
-  ds.to_csv(csv)
-
-  ds = pd.read_csv(csv)
-  ds = ds.drop(columns=['Close', 'Open', 'High', 'Low', 'Volume'])
-
-  # create training and test sets
-  # Will use last 40 values to predict 41st day price
-  # Last 30 days of the input are used for testing
-  ts = ds.iloc[:len(ds)-30, 1:2].values
-  test_set = ds.iloc[len(ds)-70:, 1:2].values
-
-  from sklearn.preprocessing import MinMaxScaler
-  sc = MinMaxScaler(feature_range = (0, 1))
-  ts = sc.fit_transform(ts)
-  test_set = sc.fit_transform(test_set)
- 
-  x_train = []
-  y_train = []
-  x_test = []
-  for i in range(40, len(ds)-30):
-    x_train.append(ts[i-40:i, 0])
-    y_train.append(ts[i, 0])
-  x_train, y_train = np.array(x_train), np.array(y_train) 
-  x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
-  for i in range(len(test_set) - 30, len(test_set)):
-    x_test.append(test_set[i-40:i, 0])
-  x_test = np.array(x_test) 
-  x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
-
-  from keras.models import Sequential
-  from keras.layers import Dense
-  from keras.layers import LSTM
-  from keras.layers import Dropout
-
-  #creates the LSTM model
-
-  regressor = Sequential()
-  regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (x_train.shape[1], 1)))
-  regressor.add(Dropout(0.2))
-  regressor.add(LSTM(units = 50, return_sequences = True))
-  regressor.add(Dropout(0.2))
-  regressor.add(LSTM(units = 50, return_sequences = True))
-  regressor.add(Dropout(0.2))
-  regressor.add(LSTM(units = 50))
-  regressor.add(Dropout(0.2))
-  regressor.add(Dense(units = 1))
-  regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
-
-  regressor.fit(x_train, y_train, epochs = 100, batch_size = 32)
-
-  predicted_stock_price = regressor.predict(x_test)
-  predicted_stock_price = sc.inverse_transform(predicted_stock_price)
-
-  #Testing the model using the last 30 days of a stock.
-
-  ds['yDay'] = ds['Adj Close']
-  ds['yDay'] = ds['yDay'].shift(1)
-
-  ds2 = ds.tail(30)
-  ds2['predict'] = predicted_stock_price
-  ds2['buy'] = ds2['predict'] - ds2['yDay']
-  ds2['profit'] = ds2['Adj Close'] - ds2['yDay']
-
-  ds2.loc[ds2['buy'] <= 0.2, 'profit'] = 0
-  print(ds2)
-
-  print(ds2['profit'].sum())
-
-  print("Mean Absolute Error:", mean_absolute_error(ds2['Adj Close'], ds2['predict']))
-  print("Coefficient of Determination:", r2_score(ds2['Adj Close'], ds2['predict']))
-  
-
 # This method predicts stock prices based on 50 day moving day average and trend.
 def analyze_stock():
   sns.set_style('whitegrid')
@@ -197,6 +115,87 @@ def analyze_stock():
   print(final.describe())
   print(total)
 
+
+# This method predicts stock prices based on the previous 40 day closing prices.
+def analyze_stock_lstm():
+  stockName = input("enter stock name: ")
+
+  end = datetime.now()
+  start = datetime(end.year - 3, end.month, end.day)
+  ds = DataReader(stockName, 'yahoo', start, end)
+
+  csv = stockName + '.csv'
+
+  ds.to_csv(csv)
+
+  ds = pd.read_csv(csv)
+  ds = ds.drop(columns=['Close', 'Open', 'High', 'Low', 'Volume'])
+
+  # create training and test sets
+  # Will use last 40 values to predict 41st day price
+  # Last 30 days of the input are used for testing
+  ts = ds.iloc[:len(ds)-30, 1:2].values
+  test_set = ds.iloc[len(ds)-70:, 1:2].values
+
+  from sklearn.preprocessing import MinMaxScaler
+  sc = MinMaxScaler(feature_range = (0, 1))
+  ts = sc.fit_transform(ts)
+  test_set = sc.fit_transform(test_set)
+ 
+  x_train = []
+  y_train = []
+  x_test = []
+  for i in range(40, len(ds)-30):
+    x_train.append(ts[i-40:i, 0])
+    y_train.append(ts[i, 0])
+  x_train, y_train = np.array(x_train), np.array(y_train) 
+  x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+  for i in range(len(test_set) - 30, len(test_set)):
+    x_test.append(test_set[i-40:i, 0])
+  x_test = np.array(x_test) 
+  x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+  from keras.models import Sequential
+  from keras.layers import Dense
+  from keras.layers import LSTM
+  from keras.layers import Dropout
+
+  #creates the LSTM model
+
+  regressor = Sequential()
+  regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (x_train.shape[1], 1)))
+  regressor.add(Dropout(0.2))
+  regressor.add(LSTM(units = 50, return_sequences = True))
+  regressor.add(Dropout(0.2))
+  regressor.add(LSTM(units = 50, return_sequences = True))
+  regressor.add(Dropout(0.2))
+  regressor.add(LSTM(units = 50))
+  regressor.add(Dropout(0.2))
+  regressor.add(Dense(units = 1))
+  regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
+
+  regressor.fit(x_train, y_train, epochs = 100, batch_size = 32)
+
+  predicted_stock_price = regressor.predict(x_test)
+  predicted_stock_price = sc.inverse_transform(predicted_stock_price)
+
+  #Testing the model using the last 30 days of a stock.
+
+  ds['yDay'] = ds['Adj Close']
+  ds['yDay'] = ds['yDay'].shift(1)
+
+  ds2 = ds.tail(30)
+  ds2['predict'] = predicted_stock_price
+  ds2['buy'] = ds2['predict'] - ds2['yDay']
+  ds2['profit'] = ds2['Adj Close'] - ds2['yDay']
+
+  ds2.loc[ds2['buy'] <= 0.2, 'profit'] = 0
+  print(ds2)
+
+  print(ds2['profit'].sum())
+
+  print("Mean Absolute Error:", mean_absolute_error(ds2['Adj Close'], ds2['predict']))
+  print("Coefficient of Determination:", r2_score(ds2['Adj Close'], ds2['predict']))
   
   
   
